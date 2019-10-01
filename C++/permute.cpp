ac7b1fc3a17ee91e4408c3ad5cefba5d518c69ca
Amey Umarekar <amey08umarekar@gmail.com>
2019-10-01 16:41:58 +0530

    Generating all permutations of a string.
    Generation of all permutations is done using backtracking.

---
+++ C++/permute.cpp
@@ -0,0 +1,33 @@
+#program to generate all permutations of a given string using backtracking. 
+
+#include <iostream> 
+#include <set>
+#include <string>
+using namespace std;
+
+set<string> permute(set<string>x,string a, int l, int h){
+    if(l == h){
+        x.insert(a);
+        return x;
+    }
+    
+    for(int i=l; i<=h; i++){
+        swap(a[i],a[l]);
+        x = permute(x,a, l+1, h);
+        swap(a[i],a[l]);
+    }
+    
+    return x; 
+}
+int main()
+ {
+  string s; 
+  cin>>s; 
+  set<string>a; 
+  a = permute(a, s, 0, s.size()-1);
+  for(auto i = a.begin(); i!=a.end(); i++){
+      cout<<*i<<" ";
+  }
+  cout<<"\n";
+  return 0;
+}
