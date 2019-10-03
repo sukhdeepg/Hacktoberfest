997ae254c8a2b866dffc0e2ba6c54479e5fc8c92
NirvashPrime <37520562+NirvashPrime@users.noreply.github.com>
2019-10-03 11:56:06 -0400

    Create arrays.php

---
+++ PHP/arrays.php
@@ -0,0 +1,21 @@
+<?php
+
+$array = ['item 1'];
+
+print_r($array);
+
+// Array
+// (
+//     [0] => item 1
+// )
+
+// Add an item to the end of an array.
+$array[] = 'item 2';
+
+print_r($array);
+
+// Array
+// (
+//     [0] => item 1
+//     [1] => item 2
+// )
