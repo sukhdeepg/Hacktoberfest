fbada282b9a3ccf9bbdc448608173debad27e5a2
madarsbiss <madars.biss@gmail.com>
2019-10-04 15:14:23 +0300

    added image preload snippet in js

--- JavaScript/imagePreload.js
+++ JavaScript/imagePreload.js
@@ -2,8 +2,10 @@ var images = new Array();
 
 function preloadImages(){
     for (i=0; i < preloadImages.arguments.length; i++){
+
          images[i] = new Image();
         images[i].src = preloadImages.arguments[i];
+        
     }
 }
 
