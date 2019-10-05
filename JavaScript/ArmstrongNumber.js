37b7a256706223c7e79e8a574f2a591f947b8e3c
Kundan <iamkundankumar28@gmail.com>
2019-10-05 08:38:31 +0530

    Create ArmstrongNumber.js
    Find the armstrong numbers of 3 digits

---
+++ JavaScript/ArmstrongNumber.js
@@ -0,0 +1,20 @@
+//This is my favorite code snippet because it has a unique logic to find armstrong number
+function three_digit_armstrong_number() 
+{
+ for (var i = 1; i < 10; ++i) 
+ {
+   for (var j = 0; j < 10; ++j) 
+     {
+        for (var k = 0; k < 10; ++k)
+        {
+          var pow = (Math.pow(i,3) + Math.pow(j,3) + Math.pow(k,3));
+          var plus = (i * 100 + j * 10 +  k);
+          if (pow == plus) 
+           {     
+             console.log(pow);
+            }
+         }
+       }
+    }
+  }
+three_digit_armstrong_number();
